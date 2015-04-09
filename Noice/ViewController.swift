955f3a3d7e6302d389af66a1d20a2bfbba8f3ab3
Jason Coco <yaedaien@gmail.com>
2015-04-09 10:10:30 -0700

    Initial Commit

---
+++ Noice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noice
+//
+//  Created by Jason Coco on 4/9/15.
+//  Copyright (c) 2015 wtrsld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
